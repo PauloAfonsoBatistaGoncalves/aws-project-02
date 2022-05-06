09eec816bcad403d8ec4ec7b5ee1995df1fd6f61
Paulo Afonso <paulo.abg@hotmail.com>
2022-05-06 13:26:15 -0300

    inicio projeto

---
+++ src/test/java/br/com/aws/AwsProject01ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.aws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsProject01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
